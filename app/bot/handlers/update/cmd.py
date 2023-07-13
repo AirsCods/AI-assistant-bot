14bd883700978cc40c93551cfcd129d12a3ed9e6
Ilia Samoletov <airscods@gmail.com>
2023-07-13 15:21:48 +0400

    update cmd

--- app/bot/handlers/update/cmd.py
+++ app/bot/handlers/update/cmd.py
@@ -10,9 +10,10 @@ from config import SCRIPT_PATH, ADMIN_ID
 
 
 @dp.message(StateFilter(BotState.CHAT), Command('update'))
-async def cmd_help(message: types.Message):
+async def cmd_update(message: types.Message):
     logger.info(f'Start cmd UPDATE from user: {message.from_user.username} : {message.from_user.id}')
-    if message.from_user.id != ADMIN_ID:
+    if str(message.from_user.id) != str(ADMIN_ID):
+        logger.info(f'Not access to update ID: {message.from_user.id}')
         await message.answer("У вас нет прав для выполнения этой команды.")
         return
 
